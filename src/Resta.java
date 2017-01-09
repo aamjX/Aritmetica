25959816eae60c493812f4b95f254360b9505647
AntonioAngel <antonioangelmunjim@gmail.com>
2017-01-10 00:46:27 +0100

    Añadida clase de resta con dos metodos

---
+++ src/Resta.java
@@ -0,0 +1,13 @@
+
+public class Resta {
+	
+	public static int restaDosNumerosEnteros(int a, int b) {
+		
+		return a - b;
+	}
+
+	public static double restaDosNumeros(double a, double b) {
+		
+		return a - b;
+	}
+}
