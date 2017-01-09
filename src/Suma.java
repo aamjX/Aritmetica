cd6bbe60b75b8552a913e98ec9f76accaf3ff958
AntonioAngel <antonioangelmunjim@gmail.com>
2017-01-10 00:42:15 +0100

    Añadidos 3 metodos mas a la clase suma

--- src/Suma.java
+++ src/Suma.java
@@ -1,7 +1,34 @@
+import java.util.List;
 
 public class Suma {
+
+	public static int sumaDosNumerosEnteros(int a, int b) {
+		
+		return a + b;
+	}
+
+	public static int sumaListaNumeroEnteros(List<Integer> list) {
+		int result = 0;
+		
+		for (Integer i : list) {
+			result = result + i;
+		}
+		
+		return result;
+	}
 	
-	public static int sumaDosNumerosEnteros(int a, int b){
-		return a+b;
+	public static double sumaDosNumeros(double a, double b){
+		
+		return a + b;
+	}
+	
+	public static double sumaListaNumeros(List<Double> list){
+		double result = 0;
+		
+		for (Double d: list){
+			result = result + d;
+		}
+		
+		return result;
 	}
 }
