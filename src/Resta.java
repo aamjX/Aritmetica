4b2a2ed9470082548f497203b46ce28997f1fc11
AntonioAngel <antonioangelmunjim@gmail.com>
2017-01-11 03:17:53 +0100

    Añadiendo nuevo metodo en resta.java

--- src/Resta.java
+++ src/Resta.java
@@ -6,6 +6,10 @@ public class Resta {
 		int result = a - b;
 		return result;
 	}
+	
+	public static int restaTresEnteros(int a, int b, int c){
+		return a-b-c;
+	}
 	//No permite que salgan numeros positivos
 	public static double restaDosNumeros(double a, double b) {
 		double result = 0; 
